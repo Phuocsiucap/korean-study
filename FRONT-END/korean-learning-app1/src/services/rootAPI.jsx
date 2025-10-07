0871499393180c9051a55aa0321ff3cb4c34e503
Information-Technology-Project-Development <nguyenvanphuoc09112004@gmail.com>
2025-10-08 00:25:16 +0700

    Integrate backend API endpoint

--- FRONT-END/korean-learning-app1/src/services/rootAPI.jsx
+++ FRONT-END/korean-learning-app1/src/services/rootAPI.jsx
@@ -1,8 +1,9 @@
 import axios from 'axios';
 import Cookies from "js-cookie";
 
-const API_BASE_URL = import.meta.env.VITE_API_URL;
+// const API_BASE_URL = import.meta.env.VITE_API_URL;
 
+const API_BASE_URL ="https://korean-study-yo4g.onrender.com/api"
 export const apiCall = async (method, endpoint, data = null, config = {}) => {
   try {
     // Lấy CSRF token tại thời điểm gọi request (luôn mới nhất)
